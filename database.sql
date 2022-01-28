9581d20934c7a06fee6f6089d2114daf490858b1
aaalace <tyj5resd@gmail.com>
2022-01-29 00:46:22 +0300

    db

--- database.sql
+++ database.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE pernproject;
 
-CREATE TABLE todos(
+CREATE TABLE todo(
     id INT PRIMARY KEY,
     title TEXT,
     pages INT,
     completed BOOLEAN default 'FALSE',
-    ren BOOLEAN default 'FALSE'
-)   
+    ren BOOLEAN default 'FALSE',
+    description TEXT
+);
