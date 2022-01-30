a03b5335ba39274ddc284fadbd809379584515c4
aaalace <tyj5resd@gmail.com>
2022-01-30 20:16:29 +0300

    fesgth

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "client/build")))
+// app.use(express.static(path.join(__dirname, "client/build")))
 
 if(process.env.NODE_ENV == "production"){
     // server static content
